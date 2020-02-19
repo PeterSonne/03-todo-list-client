255d57e9b0ae8ce5bce8b612e7243cb3f0d94bd7
Sören Peterson <papa@deep2chill.de>
2020-02-19 02:30:06 -0500

    Added some testing of DB/API responses

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -33,6 +33,11 @@ class TodoList extends React.Component {
         name: this.state.addInput
       })
       .then(res => {
+        // check if response is okay
+        if (res.status !== 200 || !res.data._id) {
+          console.log(`New Item could not be create!`);
+          return false;
+        }
         let todos = this.state.todos;
         todos.unshift(res.data);
         this.setState({ todos: todos, addInput: "" });
@@ -48,6 +53,17 @@ class TodoList extends React.Component {
     axios
       .patch(`${process.env.REACT_APP_API}/items/${id}`, item)
       .then(res => {
+        // check if response is okay
+        if (
+          res.status !== 200 ||
+          res.data._id !== id ||
+          res.data.done !== item.done
+        ) {
+          console.log(
+            `Ups - something didn't work changing ${id}. Maybe there is public holidays in MongoLand?`
+          );
+          return false;
+        }
         //update only the right item:
         let todos = this.state.todos.map(e => {
           if (e._id === res.data._id) {
@@ -56,6 +72,10 @@ class TodoList extends React.Component {
           return e;
         });
         this.setState({ todos });
+        // check if state really was updated
+        if (item.done !== this.state.todos.find(e => e._id === id).done) {
+          console.log(`Error toggling status of ${id}`);
+        }
       })
       .catch(err => console.log(err));
   };
@@ -64,7 +84,9 @@ class TodoList extends React.Component {
     axios
       .delete(`${process.env.REACT_APP_API}/items/${id}`)
       .then(res => {
-        if (res.status !== 200) {
+        // check if response is okay
+        if (res.status !== 200 || res.data._id !== id) {
+          console.log(`There was an Error trying to delete ${id}`);
           return false;
         }
         // remove Item from todo-List in state
